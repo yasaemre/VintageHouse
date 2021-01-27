1233e5aa0c06a9b9d423cc135f3e7122fb9af1b8
Emre Yasa <emreyasa86@gmail.com>
2021-01-26 17:02:47 -0800

    Create Book VC and layout objects

---
+++ VintageHouse/Controller/BookViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookViewController.swift
+//  VintageHouse
+//
+//  Created by Emre Yasa on 1/26/21.
+//  Copyright © 2021 Home. All rights reserved.
+//
+
+import UIKit
+
+class BookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
